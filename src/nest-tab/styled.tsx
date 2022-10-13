9c9d9f7be89786be793713558869b2ad9172ddc2
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-10-14 00:11:37 +0800

    refactor: scss to styled components

---
+++ src/nest-tab/styled.tsx
@@ -0,0 +1,18 @@
+import { Tabs } from '@douyinfe/semi-ui';
+import { TabsProps } from '@douyinfe/semi-ui/lib/es/tabs';
+import styled from 'styled-components';
+
+export const WrapperStyled = styled.div`
+  width: 100%;
+`;
+
+export const DocsWrapprStyled = styled.div`
+  margin-top: 8px;
+`;
+
+export const SemiTabsStyled = styled(Tabs)<TabsProps>`
+  .semi-tabs-bar-button {
+    display: flex;
+    justify-content: center;
+  }
+`;
